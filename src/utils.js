1b9a4ff9806c84c5e8eafeabf4f8789515e91a37
ohnapaula <ohnapaula@gmail.com>
2025-09-06 13:56:17 -0300

    refactor(utils): melhoria na verificação de aluno

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@ function verificarAluno(nome) {
         return false;
     } else {
         for (let i = 0; i < alunos.length; i++) {
-            if (alunos[i].nome === nome) {
+            if (alunos[i].nome.trim().toLowerCase() === nome.trim().toLowerCase()) {
                 return true;
             }
         }
