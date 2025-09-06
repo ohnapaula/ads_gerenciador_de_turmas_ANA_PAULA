a9cb08b866034f8757f8799380518771c30c96f8
ohnapaula <ohnapaula@gmail.com>
2025-09-06 18:39:38 -0300

    feat(utils): calcula média de um aluno

--- src/utils.js
+++ src/utils.js
@@ -83,4 +83,23 @@ function adicionarNotas(nome, notasArray) {
     }
 }
 
-module.exports = { adicionarAluno, listarAlunos, adicionarNotas, verificarNotas, verificarAluno, adicionarNotas };
+function calcularMedia(nome) {
+    let ehAluno = verificarAluno(nome);
+    let posicaoDoAluno = buscarAluno(nome);
+    let soma = 0;
+    let media = 0;
+
+    if (ehAluno && alunos[posicaoDoAluno].notas === undefined) {
+        console.log("AVISO: Aluno sem notas.");
+        return null;
+    } else if (ehAluno && alunos[posicaoDoAluno].notas !== undefined) {
+        alunos[posicaoDoAluno].notas.forEach(nota => soma += nota);
+        media = soma / alunos[posicaoDoAluno].notas.length;
+        console.log(`Média de ${alunos[posicaoDoAluno].nome}: ${media.toFixed(2)}`);
+        return media.toFixed(2);
+    } else {
+        console.log("AVISO: Tentativa de cálculo de média para aluno não cadastrado.");
+        return null;
+    }
+}
+module.exports = { adicionarAluno, listarAlunos, adicionarNotas, verificarNotas, verificarAluno, adicionarNotas, calcularMedia };
